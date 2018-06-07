ddd3529fe9838f07646504163abab1db55877c3d
Erich EJ Best <waterskiingguy@gmail.com>
2018-06-07 09:27:48 -0400

    updated removed error checking

--- files/airflow_user_data.sh
+++ files/airflow_user_data.sh
@@ -1,40 +1,38 @@
 #cloud-config
 echo "##################################################################################"
-echo "##### EJ's user_data.sh ##########################################################"
-echo "##################################################################################"
-touch /tmp/ej_was_here.txt
-echo "ej_was_inside_the_file" >> /tmp/ej_was_here.txt
+echo "##### airflow_user_data.sh #######################################################"
 echo "##################################################################################"
 #package_update: true
 #package_upgrade: true
 #packages:
+echo "##################################################################################"
+echo "##### sudo yum upgrade python-pip jq bc git httpd6 php56-mysqlnd httpd ###########"
+echo "##################################################################################"
 sudo yum upgrade python-pip jq bc git httpd6 php56-mysqlnd httpd
-  echo "##################################################################################"
-  echo "##### copy keys ##################################################################"
-  echo "##################################################################################"
-  sudo cp /home/ec2-user/.ssh/ej_key_pair.pem /root/.ssh/ej_key_pair.pem
-  sudo chmod -v 400 /root/.ssh/*
-
+echo "##################################################################################"
+echo "##### copy keys ##################################################################"
+echo "##################################################################################"
+sudo cp /home/ec2-user/.ssh/ej_key_pair.pem /root/.ssh/ej_key_pair.pem
+sudo chmod -v 400 /root/.ssh/*
+echo "##################################################################################"
+echo "##### sudo yum install -y gcc-c++ python-devel python-setuptools #################"
+echo "##################################################################################"
 sudo yum install -y gcc-c++ python-devel python-setuptools
-
-sudo pip install --upgrade pip; RC=$?
-if [[ $RC != 0 ]]; then
-  echo "FAILURE! RC=$RC There was a problem with --upgrade pip;"
-  exit 3;
-fi
-
-sudo /usr/local/bin/pip install pyserial; RC=$?
-if [[ $RC != 0 ]]; then
-  echo "FAILURE! RC=$RC There was a problem with pyserial;"
-  exit 3;
-fi
-
-sudo /usr/local/bin/pip install PrettyTable; RC=$?
-if [[ $RC != 0 ]]; then
-  echo "FAILURE! RC=$RC There was a problem with PrettyTable;"
-  exit 3;
-fi
-
+echo "##################################################################################"
+echo "##### sudo pip install --upgrade pip #############################################"
+echo "##################################################################################"
+sudo pip install --upgrade pip
+echo "##################################################################################"
+echo "##### sudo /usr/local/bin/pip install pyserial ###################################"
+echo "##################################################################################"
+sudo /usr/local/bin/pip install pyserial
+echo "##################################################################################"
+echo "##### sudo /usr/local/bin/pip install PrettyTable ################################"
+echo "##################################################################################"
+sudo /usr/local/bin/pip install PrettyTable
+echo "##################################################################################"
+echo "##### sudo /usr/local/bin/pip install oauth ######################################"
+echo "##################################################################################"
 sudo /usr/local/bin/pip install oauth
 echo "##################################################################################"
 echo "##### sudo /usr/local/bin/pip install cheetah ####################################"
@@ -44,6 +42,9 @@ echo "##########################################################################
 echo "##### sudo /usr/local/bin/pip install argparse ###################################"
 echo "##################################################################################"
 sudo /usr/local/bin/pip install argparse
+echo "##################################################################################"
+echo "##### sudo /usr/local/bin/pip install --ignore-installed airflow[s3,hive,python]##"
+echo "##################################################################################"
 sudo /usr/local/bin/pip install --ignore-installed airflow[s3,hive,python]
 echo "##################################################################################"
 echo "##### airflow initdb #############################################################"
